80491a90fa5988b48438a7dc1521ddcb3a5a844a
reychristopherposadas <reychristopherposadas@gmail.com>
2025-10-08 20:31:57 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Matthews
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Matthews" };
+        }
+    }
+}
